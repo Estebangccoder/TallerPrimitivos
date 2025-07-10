ea576b0a5626127db2091e43ae49fd0144305310
estebangccoder <estebangccoder@gmail.com>
2025-07-10 16:17:31 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// Ejercicio 1
+
+const nombre = String("Edson Esteban Garcia Cuesta");
+let edad = Number(32);
+let esEstudiante=Boolean(true);
+
+
+// Ejercicio 2
+
+let variableVacia = undefined;
+
+let variableNull = null;
+
+console.log(variableVacia);
+console.log(variableNull);
+
+
+//2. Ingreso de datos por teclado
+
+let anoNacimiento =Number(prompt("Ingresa año de nacimiento"));
+let age= 2025-number;
+
+alert(`Usted debde tener alrededor de ${age} años`);
+
+//2
+
+let number1=Number(prompt("Ingresa el primer número"));
+let number2=Number(prompt("Ingresa el segundo número"));
+
+let suma = number1 +number2
+alert(`La suma de los número es ${suma}`)
+
+
+// 3. Condicional
+
+let age2 =Number(prompt("Ingresa tu edad"));
+
+if(age2 >= 18){
+    console.log("Puedes entrar");
+}
+else{
+    console.log("No puedes ingresar")
+}
+
+
+let numberConsola = Number(input("Digita un número"))
+
+
